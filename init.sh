b7ff427c6d179523dce441c153563185debcffb4
yacudzer <yakubov@aiislab.com>
2022-07-04 00:51:59 +0300

    Модифицирована роль добавления пользователя ansible

--- init.sh
+++ init.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+#!/bin/bash
+if [ $# == 0 ]; then
+        echo "No params. Please type hostname or IP address of your machine"
+        exit 1
+fi
+
 ansible-playbook --vault-password-file ./.secrets/vault-password init.yaml
 
