f139fd1fb11f8140c931929e799c1d3dbe674fb6
yacudzer <yakubov@aiislab.com>
2022-12-19 22:06:35 +0300

    Обновлен init.sh: теперь он обновляет ssh-отпечаток ключа у шаблонной машины

--- init.sh
+++ init.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 templatehostname="debiantemplate"
+domainname="blu.local"
 
 if [[ $# == 0 ]]; then
         echo "No params. Please type NEW hostname for $templatehostname"
         exit 1
 fi
 
+ssh-keygen -f ~/.ssh/known_hosts -R $templatehostname.$domainname
+ssh-keyscan -H $templatehostname.$domainname >> ~/.ssh/known_hosts
 ansible-playbook --vault-password-file ./.secrets/vault-password init.yaml --extra-vars "new_hostname=$1"
 
