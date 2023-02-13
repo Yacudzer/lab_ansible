27aa82c0665406650261b81cec7c170e2a12fd53
yacudzer <yakubov@aiislab.com>
2023-02-13 08:31:34 +0300

    shutdown modified

--- allshutdown.sh
+++ allshutdown.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-if [[ "$USER" != "root" ]]; then
-	echo run with root rights only!
-	exit 1
-fi
 cd `dirname ${BASH_SOURCE}`
 pwd
-sudo -u ansible ansible-playbook allshutdown.yaml
-shutdown now
+ansible-playbook allshutdown.yaml
+sudo shutdown now
 
